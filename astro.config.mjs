8bf211b8839e3bbf5b7f01d71b824a876831e273
Sergio <sergio-exposito@hotmail.com>
2025-09-19 15:30:34 +0200

    cambiado diseño portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [],
   output: "server",
+
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
